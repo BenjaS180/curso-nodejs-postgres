7dd7873f4a5b8006cfb1a6419a4331b35a5a72fe
BSbenjaminsoto <benjaminsoto661@hotmail.com>
2024-05-14 23:16:50 -0400

    proyecto base

--- services/user.service.js
+++ services/user.service.js
@@ -1,18 +1,25 @@
 const boom = require('@hapi/boom');
 
+const pool = require('../libs/postgres.pool.js')
+
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool
+    this.pool.on('error', (err)=> console.error(err))
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    return [];
+    const query = 'SELECT * FROM tasks';
+    const response = await this.pool.query(query)
+    return response.rows
   }
 
   async findOne(id) {
-    return { id };
+    const client = await getConnection();
   }
 
   async update(id, changes) {
